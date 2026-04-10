d30a562b8cee4c0cf73cfb2bea08812bf0e44f65
Aniruddha Mahamuni <aniruddham27@gamil.com>
2026-04-10 09:39:50 +0530

    6-cdn added

---
+++ 10-cdn/index.js
@@ -0,0 +1,32 @@
+function fetchFromOrigin() {
+    return new Promise((resolve) => {
+      console.log("Fetching from main server (far)...");
+      setTimeout(() => resolve("Data from origin"), 2000);
+    });
+  }
+  
+  let cdnCache = null;
+  
+  async function fetchFromCDN() {
+    if (cdnCache) {
+      console.log("Serving from CDN (near) ⚡");
+      return cdnCache;
+    }
+  
+    console.log("CDN MISS ❌");
+    const data = await fetchFromOrigin();
+    cdnCache = data;
+    return data;
+  }
+  
+  async function run() {
+    console.time("First Request");
+    await fetchFromCDN(); // MISS → slow
+    console.timeEnd("First Request");
+  
+    console.time("Second Request");
+    await fetchFromCDN(); // HIT → fast
+    console.timeEnd("Second Request");
+  }
+  
+  run();
