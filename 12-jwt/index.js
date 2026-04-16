070cf3ad8d29a312c110a3df9989ecf0bfb01afb
Aniruddha Mahamuni <aniruddham27@gamil.com>
2026-04-16 10:20:41 +0530

    9-jwt demo added

---
+++ 12-jwt/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+
+const SECRET = "mysecretkey";
+
+// Login route
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+
+  const token = jwt.sign({ username }, SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
+// Protected route
+app.get("/protected", (req, res) => {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).send("Token missing");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    res.send(`Welcome ${decoded.username}`);
+  } catch (err) {
+    res.status(403).send("Invalid token");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
