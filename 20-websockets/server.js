67f6a5f1ac8151b400f37154dcd7ae0f143613fd
Aniruddha Mahamuni <aniruddham27@gamil.com>
2026-05-31 12:51:22 +0530

    24-updated websocket

--- 20-websockets/server.js
+++ 20-websockets/server.js
@@ -5,25 +5,43 @@ const WebSocket = require("ws");
 const app = express();
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
 
+let clientCounter = 1;
+
 wss.on("connection", (ws) => {
-  console.log("Client connected");
+  const clientId = clientCounter++;
+
+  ws.clientId = clientId;
+
+  console.log(`Client-${clientId} connected`);
+
+  ws.send(
+    JSON.stringify({
+      type: "welcome",
+      clientId,
+    })
+  );
 
   ws.on("message", (message) => {
-    console.log("Received:", message.toString());
+    console.log(
+      `Received from Client-${clientId}: ${message.toString()}`
+    );
 
-    // Broadcast to all clients
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
+        client.send(
+          JSON.stringify({
+            from: `Client-${clientId}`,
+            message: message.toString(),
+          })
+        );
       }
     });
   });
 
   ws.on("close", () => {
-    console.log("Client disconnected");
+    console.log(`Client-${clientId} disconnected`);
   });
 });
 
