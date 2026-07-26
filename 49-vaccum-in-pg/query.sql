fcca4a62e2069dfc553822d45e80c7e8e133fc76
Aniruddha Mahamuni <aniruddham27@gamil.com>
2026-07-26 12:54:11 +0530

    49-vacuum completed

---
+++ 49-vaccum-in-pg/query.sql
@@ -0,0 +1,46 @@
+-- Create users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+-- Insert users
+INSERT INTO users(name)
+SELECT
+'User ' || gs
+FROM generate_series(1,100000) gs;
+
+-- Table size before vacuum
+SELECT pg_size_pretty(pg_relation_size('users')) AS size_before_vacuum;
+
+-- Delete half of the users
+DELETE FROM users WHERE id >= 100;
+
+-- Table size after delete
+SELECT pg_size_pretty(pg_relation_size('users')) AS size_after_delete;
+
+-- SHOW DEAD TUPLES
+SELECT
+    n_live_tup,
+    n_dead_tup
+FROM pg_stat_user_tables
+WHERE relname = 'users';
+
+-- Run vacuum to reclaim space
+VACUUM ANALYZE users;
+
+-- SHOW DEAD TUPLES AFTER VACUUM
+SELECT
+    n_live_tup,
+    n_dead_tup
+FROM pg_stat_user_tables
+WHERE relname = 'users';
+
+-- Table size after vacuum
+SELECT pg_size_pretty(pg_relation_size('users')) AS size_after_vacuum;
+
+-- Instead of growing, PostgreSQL reuses the dead space.
+INSERT INTO users(name)
+SELECT
+'New User'
+FROM generate_series(1,100);
